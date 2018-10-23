6b2f3f4e1380049d3a37cc26d3d29f5f6d7d9d43
ErikPapyan <e-papyan@bk.ru>
2018-10-23 16:34:11 +0400

    added js

---
+++ Erik/js/23.10.2018/main.js
@@ -0,0 +1,54 @@
+
+
+/*kent zuyg*/
+
+// let masiv = [1,2,11,4,3,6,9,8,5,10,13,12,7];
+// for (var i in masiv) {
+// 	if (masiv[i] % 2 === 0) {
+// 		var a = masiv.splice(i,1);
+// 		var b = a[0];
+// 		masiv.unshift(b);
+// 	}else{
+// 		var abg = masiv.splice(i,1);
+// 		var abgd = abg[0];
+// 		masiv.push(abgd); 
+// 	}
+// }
+// console.log(masiv);
+
+
+/*mijin tvabanakan*/
+// const masiv = [7,65,9,7850];
+// let mv = 0;
+// for (let i of masiv) {
+// 	mv += i;
+// }
+// mv = mv / masiv.length;
+// console.log(mv);
+
+
+
+/*tars bar*/
+// let a = "123456789";
+// let b = a.length;
+// let c=[];
+// for (let i = b - 1; i >= 0; i--) {
+// 	c += a[i];
+// }
+// console.log(c);
+
+
+
+/*ajman kargov*/
+// const array = [1,9,25,62,89,-6000,600,25];
+
+// for (var i = 0; i < array.length; i++) {
+// 	for (var j = 0; j < array.length; j++) {
+// 		if(array[i] < array[j]){ 
+// 			array[i] = array[i] + array[j];
+// 			array[j] = array[i] - array[j];
+// 			array[i] = array[i] - array[j];	
+// 		}
+// 	}
+// }
+// console.log(array);
