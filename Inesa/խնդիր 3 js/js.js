e830ea29c7f0d425a3a55ab7419771ff62392326
InesaMkrtchyan <44000734+InesaMkrtchyan@users.noreply.github.com>
2018-10-23 19:43:30 +0400

    Add files via upload

--- Inesa/խնդիր 3 js/js.js
+++ Inesa/խնդիր 3 js/js.js
@@ -1,17 +1,12 @@
 var a = [33, 6, 7, 8, 9, 55, 95, 69];
-function bubleSort(a){
-    var sawapped;
-    do{
-        swapped = false;
-        for(var i = 0; i < a.length - 1; i++)
-            {
-                if (a[i] > a[i+1])
-                    {
-                        var temp = a[i];
-                        a[i] = a[i+1];
-                        a[i+1] = temp ; 
-                        swapped = true;
-                    }
-            }
-    }while(swapped);
+for(var i = 0; i < 8; i++)
+{
+	if (a[i] > a[i+1])
+		{
+			var temp = a[i];
+			a[i] = a[i+1];
+			a[i+1] = temp ; 
+			swapped = true;
+		}
+		else{}
 }
