9b843c335e024c4bcb2c5c94d85de1bf6cb82d44
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2018-10-24 12:15:14 +0400

    use tab

--- Edgar/JsWork/array_2.js
+++ Edgar/JsWork/array_2.js
@@ -3,14 +3,14 @@ const massiv = [];
 let len, k;
 let j = 0;
 for (let i = 0; i < 10; i++) {
- 	massiv[i] = [];
- for (j = 9; j >= i; j--) {
- 	massiv[i][j] = 1; 
- 	for (k = 0; k <= i; k++) {
- 	massiv[i][k] = 0; 
-	}
- }
+    massiv[i] = [];
+    for (j = 9; j >= i; j--) {
+        massiv[i][j] = 1;
+        for (k = 0; k <= i; k++) {
+            massiv[i][k] = 0;
+        }
+    }
 }
 
- console.log(massiv);
+console.log(massiv);
   
