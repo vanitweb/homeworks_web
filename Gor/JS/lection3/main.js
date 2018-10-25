8aab8d88527eefd21ba9f2da7ee5faadd67a9ed0
Gor Manukyan <gorman93@mail.ru>
2018-10-25 20:42:21 +0400

    add a lection3

---
+++ Gor/JS/lection3/main.js
@@ -0,0 +1,28 @@
+let txt = ["abc", "qwerty", "asdfgh", "zxcvbn"];
+function revers(a) {
+	let newTxt = "";
+	for(let i = a.length - 1; i >= 0; i--) {
+		newTxt += a[i];
+	}
+	return newTxt;
+}
+for(i = 0, len = txt.length; i < len; i++) {
+	console.log(revers(txt[i]));
+}
+//calculator
+let calc = function(x, y, z) {
+	switch(z) {
+		case '+':
+		return x + y;
+		case '-':
+		return x - y;
+		case '*':
+		return x * y;
+		case '/':
+		return x / y;
+	}
+}
+console.log(calc(1, 2, '+'));
+console.log(calc(1, 2, '-'));
+console.log(calc(1, 2, '*'));
+console.log(calc(1, 2, '/'));
