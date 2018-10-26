da653b818ff61b8632619f86d413fecfd0232712
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2018-10-26 10:29:32 +0400

    from notebook

---
+++ Edgar/JsWork/array_2.js
@@ -0,0 +1,12 @@
+const arr = [];
+let len,k;
+for (let i = 0; i<10; i++){
+ arr[i] = [];
+}
+
+for (let j = 0, k = arr.length,  len = arr.length; j<len; j++){
+arr[j][k] = 1;
+console.log(arr);
+k--;
+}
+/*console.log(k);*/
