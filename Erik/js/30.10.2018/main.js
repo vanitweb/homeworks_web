8cc446973f34ed5a999f937e7f79226ff427789d
ErikPapyan <e-papyan@bk.ru>
2018-10-30 19:16:54 +0400

    asss

---
+++ Erik/js/30.10.2018/main.js
@@ -0,0 +1,62 @@
+//function tiv(a,b) {
+// console.log(a + b);
+// return tiv();
+// function tiv() {
+//     return a - b;
+// }
+//}
+//console.log(tiv(75,6));
+//let fak = 1;
+//function faktorial() {
+//	let tiv = 10;
+//	for (let i = 1; i <= 10; i++) {
+//		fak *= i;
+//	}
+//	console.log(fak);
+//	return fak;
+//}
+//setTimeout(faktorial, 10000);
+//console.log(fak);
+//
+//let a = setInterval(function(){ 
+//	if (fak === 1) {
+//		console.log(fak,"stuguma");
+//	}else{
+//		console.log(fak,"patasxan");
+//		clearInterval(a);
+//	}
+//}, 2000);
+//
+//
+
+
+//
+//let a = 7, b = 9,arjeq;
+//function gumarum() {
+//    arjeq = a + b;
+//    console.log(arjeq);
+//    return arjeq;
+//}
+//setTimeout(gumarum, 2000);
+//let interval1 = setInterval(function(){ 
+//    if (arjeq !== undefined) {
+//        clearInterval(interval1);
+//        setTimeout(function(){arjeq = arjeq - a;console.log(arjeq);return arjeq;}, 1000);
+//    }
+//}, 100);
+//let interval2 = setInterval(function(){ 
+//    if (arjeq === b) {
+//        clearInterval(interval2);
+//        setTimeout(function(){arjeq = arjeq / 2;console.log(arjeq);return arjeq;}, 1000);
+//    }
+//}, 100);
+
+
+
+
+
+
+
+
+
+
