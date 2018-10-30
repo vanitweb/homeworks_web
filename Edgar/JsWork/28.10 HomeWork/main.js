ba22c79ed758e1090c6f964a2de246e6ca35a171
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2018-10-30 17:28:01 +0400

    30.10

--- Edgar/JsWork/28.10 HomeWork/main.js
+++ Edgar/JsWork/28.10 HomeWork/main.js
@@ -1,64 +1,66 @@
 /*console.log("Home work 1");
-function numb(){
-    function numMid(n){
+
+function numb() {
+    function numMid(n) {
         let k = n.arguments.length;
-        return count/k;
+        return count / k;
     }
     let i;
     let count = 0;
-    for(i = 0; i < arguments.length; i++){
+    for (i = 0; i < arguments.length; i++) {
         count += arguments[i];
     }
-       return (`Գումարը հավասար է - ${count}, \nՄիջին տվաբանականը ~ ${Math.round(numMid(numb))}`);
+    return (`Գումարը հավասար է - ${count}, \nՄիջին տվաբանականը ~ ${Math.round(numMid(numb))}`);
 
 }
 
-console.log(numb(5,5,10,20,14,58,7,4));
+console.log(numb(5, 5, 10, 20, 14, 58, 7, 4));*/
 console.log("---------------------------");
 console.log("Home work 2");
 let c;
 function factorial() {
     let num = 5;
     let f = 1;
-  if (num === 0 || num === 1){
-      console.log(1);
-  }
-    for (i = 2; i <= num; i++){
+    if (num === 0 || num === 1) {
+        console.log(1);
+    }
+    for (i = 2; i <= num; i++) {
         f *= i;
-  }
+    }
     console.log(f);
 }
-function check(){
+
+function check() {
     c = factorial();
-} 
-setTimeout(check,10000); 
+}
+setTimeout(check, 10000);
 let counter = 0;
-let setI = setInterval(function checkUnd(){
+let setI = setInterval(function checkUnd() {
     counter += 1;
-    if(counter === 10){
+    if (counter === 10) {
         clearInterval(setI);
         console.log("The end of")
     }
-    if(c !== undefined){
+    if (c !== undefined) {
         console.log(c);
-    }else{  
+    } else {
         console.log("Processing...");
     }
-},2000);*/
+}, 2000);
+
+
 console.log("---------------------------");
 console.log("Home work 3");
-function mainF(a,b){
-setTimeout(function counter(){
-    console.log(a+b);
-},2000);
-setTimeout(function counter(){
-     console.log(a-b);
-},3000); 
-setTimeout(function counter(){
-    console.log(a/b);
-},4000); 
+function mainF(a, b) {
+    setTimeout(function counter() {
+        console.log(a + b);
+    }, 2000);
+    setTimeout(function counter() {
+        console.log(a - b);
+    }, 3000);
+    setTimeout(function counter() {
+        console.log(a / b);
+    }, 4000);
     return (`Start:\nFirst number is: ${a}\nSecond number is: ${b}`);
 }
-console.log(mainF(56,10));
-
-
+console.log(mainF(56, 10));
