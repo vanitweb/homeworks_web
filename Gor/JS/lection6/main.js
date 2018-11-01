2b8f4078e05098543d7a677fd1a255dde9b4ace1
Gor Manukyan <gorman93@mail.ru>
2018-11-01 20:51:30 +0400

    xnditnern avelacreci

--- Gor/JS/lection6/main.js
+++ Gor/JS/lection6/main.js
@@ -1,1 +1,41 @@
-//comment
+//xndir: funkciayin poxancel tiv ev amen kanchi jamanak naxordi hamemat argument@ avelacnel 1-ov
+function newFunction(argument) {    
+    return function returnFunc() {
+        argument++;
+        console.log(argument);
+    }
+}
+let f2 = newFunction(4);
+f2(); // 5
+f2(); // 6
+f2(); // 7
+//moo - foo functions
+(function() {
+    let bar = 4;
+    function foo(x) {
+        let baz = 3;
+        return function (y) {
+            console.log(x + y + (++bar) + (++baz));
+        }
+    }
+    let moo = foo(2);
+    moo(1);
+    moo(1);
+})();
+//funkcian 5 angam kancheluc heto kanchel ayl funkcia
+function callater(func, count) {
+    let a = 0;
+    return function callManager() {
+        a++;
+        console.log("Hey everyone");
+        if(a === count) {
+            func();
+        }
+    }    
+        
+}
+function print() {
+    console.log("Hellow World");
+}
+let f = callater(print, 5);
+f(); f(); f(); f(); f();
