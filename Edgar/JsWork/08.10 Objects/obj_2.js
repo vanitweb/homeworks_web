2a201c37c3080de302a278614ceab6e45617fb22
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2018-11-08 20:40:28 +0400

    new

--- Edgar/JsWork/08.10 Objects/obj_2.js
+++ Edgar/JsWork/08.10 Objects/obj_2.js
@@ -1,5 +1,5 @@
 //METHOD this
-const ma = {
+/*const ma = {
     "keye" : "value",
      getKey:function(){
          this.keye = "Vavle";
@@ -11,8 +11,21 @@ const ma1 = {
 };
 
 let data =ma.getKey();
-ma1.getkey  = ma.getKey;
 
 console.log(ma.getKey());
 console.log(ma1.getkey());
-console.log(ma.keye);
+console.log(ma.keye);*/
+
+let x = 3;
+let foo ={
+    x: 2,
+        bar:{
+            x:1,
+            bar:function(){
+                return this.x;
+            }
+        }
+};
+let go = foo.bar.bar;
+console.log(go());// "use script"-ov ktpi 3, vercnelov global x-y, else - undefined;
+console.log(foo.bar.bar()); // => 1
