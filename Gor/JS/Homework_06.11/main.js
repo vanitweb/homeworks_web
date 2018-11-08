6cb7301833d6fc5720a882ec139a818753927c42
gorman93 <gorman93@mail.ru>
2018-11-08 16:31:34 +0400

    erkrord xndirn el a ashxatum

--- Gor/JS/Homework_06.11/main.js
+++ Gor/JS/Homework_06.11/main.js
@@ -11,24 +11,23 @@ function printBooks(read) {
 	}
 }
 printBooks(false);
-//erku obyektneri tarberutyunner@ (chi ashxatum((((   )
+//erku obyektneri tarberutyunner@
 function diffObject(obj1, obj2) {
-	let len;
 	let obj3 = {};
-	if(obj1.length >= obj2.length) {
-		len = obj1.length;
-	} else {
-		len = obj2.length;
-	}
-	for(let i = 0; i < len; i++) {
-		if(obj1[i] != obj2[i]) {
-			obj3.i = obj1[i];
-			obj3.i = obj2[i];
+	let keysOfObj1 = Object.keys(obj1);
+	for(let i = 0, len = keysOfObj1.length; i < len; i++) {
+		if(obj2[keysOfObj1[i]]) {
+			delete obj2[keysOfObj1[i]];
+		} else {
+			obj3[keysOfObj1[i]] = obj1[keysOfObj1[i]];
 		}
+	}
+	for(let item in obj2) {
+		obj3[item] = obj2[item];
 	}
 	console.log(obj3);
 }
-diffObject({a: 2, b: 2, c: 3, e: 4}, {a: 2, b: 2, c: 3, d: 4});
+diffObject({a: 2, b: 2, c: 3, k: 1, p: 5, e: 4}, {a: 2, b: 2, c: 3, h: 2, d: 4});
 //shrjel obyekt@
 function reverseObject(obj) {
 	let ietm;
