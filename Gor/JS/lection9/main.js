5f2ca435d040e33ffe33322f83d480bd02165801
Gor Manukyan <gorman93@mail.ru>
2018-11-11 01:14:36 +0400

    added a homework_11.11 folder and update classwork

--- Gor/JS/lection9/main.js
+++ Gor/JS/lection9/main.js
@@ -1,36 +1,78 @@
 //Number, Date, String, Math metodner
+//trvac erkarutyamb patahakan teqsti generacum
+function randomstring(len) {
+	let str = '';
+  	let randomchar = function() {
+    let n = Math.floor(Math.random() * 62);
+    if (n < 10) {
+		return n; //1-10
+	}
+    if (n < 36) {
+		return String.fromCharCode(n + 55); //A-Z
+	}
+	return String.fromCharCode(n + 61); //a-z
+  	}
+  	while (str.length < len) {
+		str += randomchar();
+	}
+  	return str;
+}
+console.log(randomstring(15));
+//XXXnn-xxxnn-xxxnn formati patahakan toxi generacum
 function randomString(len) {
 	let str = '';
 	let randomNumber = function() {
-		return Math.floor(Math.random() * 10);
+		return Math.floor(Math.random() * 10); //1-10
 	}
 	let randomChar = function() {
     	let n = Math.floor(Math.random() * 25);
-			return String.fromCharCode(n + 97);
+			return String.fromCharCode(n + 97); //a-z
 	}
 	let randomCharUp = function() {
     	let n = Math.floor(Math.random() * 25);
-			return String.fromCharCode(n + 65);
+			return String.fromCharCode(n + 65); //A-Z
 	}
-	let count = Math.floor(len / 5);
-	for(let i = 0; i < 3; i++) {
+	if(len <= 3) {
+		for(let i = 1; i <= len; i++) {
+			str += randomCharUp();
+		}
+		return str;
+	}
+	if(len <= 5) {
+		for(let i = 1; i <= len - 2; i++) {
+			str += randomCharUp();
+		}
+		for(let i = 1; i <= len - 3; i++) {
+			str += randomNumber();
+		}
+		return str;
+	}
+	for(let i = 1; i <= 3; i++) {
 		str += randomCharUp();
 	}
-	for(let i = 0; i < 2; i++) {
+	for(let i = 1; i <= 2; i++) {
 		str += randomNumber();
 	}
 	str += '-';
-	while (str.length < len + count - 1) {
-		for(let i = 0; i < 3; i++) {
+	let count = Math.ceil(len / 5) - 1;
+	while (str.length < len + count) {
+		for(let i = 1; i <= 3; i++) {
+			if(str.length === len + count) {
+				break;
+			}
 			str += randomChar();
 		}
-		for(let i = 0; i < 2; i++) {
+		for(let i = 1; i <= 2; i++) {
+			if(str.length === len + count) {
+				break;
+			}
 			str += randomNumber();
 		}
-		if(str.length === len + (count - 2)) {
+		if(str.length === len + count) {
 			break;
+		} else {
+			str += '-';
 		}
-		str += '-';
   	}
   	return str;
 }
