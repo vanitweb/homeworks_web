15048558c9840b124332dfb655eb0ae4e50ca671
Gor Manukyan <gorman93@mail.ru>
2018-11-12 21:43:58 +0400

    avelacvel en bolor tnayinner@ bayc der uxxumner unem anelu

--- Gor/JS/Homework_11.11/main.js
+++ Gor/JS/Homework_11.11/main.js
@@ -14,20 +14,13 @@ function makeShort(str, count) {
 console.log(makeShort("dbh ff sfs ds ehdtu", 3));
 //homework 3_1: find_matches_count("ab abc ssjdf abk ab sfrkl", "ab"); => "ab" is found 4 times
 function findMatchesCount(str, findStr) {
-	let count = 0;
-	let strArr = str.split(' ');
-	for(item of strArr) {
-		if(item.substr(0, findStr.length) === findStr) {
-			count++;
-		}
-	}
-	return count;
+	return str.toLowerCase().split(findStr).length - 1;
 }
-console.log(findMatchesCount("ab abc ssjdf abk ab sfrkl", "ab"));
+console.log(findMatchesCount("ab abc ssjdf abk ab sfAbrkl", "ab"));
 //homework 3_2: find_exact_matches_count("ab abc ssjdf abk ab sfrkl", "ab"); => "ab" is found 2 times
 function findExactMatchesCount(str, findStr) {
 	let count = 0;
-	let strArr = str.split(' ');
+	let strArr = str.toLowerCase().split(' ');
 	for(item of strArr) {
 		if(item === findStr) {
 			count++;
@@ -35,4 +28,31 @@ function findExactMatchesCount(str, findStr) {
 	}
 	return count;
 }
-console.log(findExactMatchesCount("ab abc ssjdf abk ab sfrkl", "ab"));
+console.log(findExactMatchesCount("ab abc ssjdf abk ab sfrkl ab", "ab"));
+//homework 4: fill_by_length(15, 5[, '+']); => [+]00015
+function fillByLength(num, size, sign) {
+	if(size <= num.toString().length) {
+		return num;
+	}
+	let newStr = num.toString();
+	while(newStr.length < size) {
+		newStr = '0' + newStr;
+	}
+	if(arguments[2]) {
+		newStr = arguments[2] + newStr;
+	}
+	return newStr;
+}
+console.log(fillByLength(15, 5, '+'));
+//homework 5: find_palindrome_words("Hello! Aba kdfk ckkc skroekr ababa"); => "Aba", "ckkc", "ababa"
+function findPalidromeWords(str) {
+	let strArray = str.split(' ');
+	let arr = [];
+	for(let item of strArray) {
+		if(item.toLowerCase() === item.split('').reverse().join('').toLowerCase()) {
+			arr.push(item);
+		}
+	}
+	return arr;
+}
+console.log(findPalidromeWords("Hello! Aba kdfk ckkc skroekr ababa"));
