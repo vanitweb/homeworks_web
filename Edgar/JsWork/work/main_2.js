dcd731f059b6e268357404efe64aa869c0da459a
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2018-11-13 20:57:39 +0400

    new

--- Edgar/JsWork/work/main_2.js
+++ Edgar/JsWork/work/main_2.js
@@ -1,1 +1,4 @@
-
+/*inch vor kod, stugel pakagcery, stugel bolor {}() bacvox pakvox pakagcery, ete stringi meja */
+/*araji 3 rarery mecatax heto 2 poqr heto  _, heto xxxnn_XXXnn ...*/
+/*gtnelu enq verji pakvox }-y u heto nayenq dra pakvoxy*/
+let c = n.match(/^\((?=)|[[\s+[A-Za-z0-9_\[(?=[)\]].\s+]*)]*)$/g);
