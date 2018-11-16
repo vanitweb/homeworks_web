f530af921e230c0483def7c7fc6ffe1ee56035f9
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2018-11-16 15:36:00 +0400

    HomeWork at 19.11

--- Edgar/JsWork/15.11 Arrays/main.js
+++ Edgar/JsWork/15.11 Arrays/main.js
@@ -1,4 +1,4 @@
-let arr = ["a", [12,45,7,1,20,5], "c", "d", "e", ["name1", "name2"], 25];
+/*let arr = ["a", [12,45,7,1,20,5], "c", "d", "e", ["name1", "name2"], 25];
 
 function copy(arg){
 const arrCopy = new Array();
@@ -24,4 +24,58 @@ function copy2(arg2){
     arr2 = map1;
     return arr2;
 }
-console.log("map :", copy2(arr));
+console.log("map :", copy2(arr));*/
+
+// 0. (Դասարանային) Գրել ֆունկցիա, որը կհարթեցնի տրված զանգվածը։ Օրինակ
+/*let mass = [1, [2], [3, [[4]]],[5,6]];
+function make_flat(arg){
+   return arg.reduce((acc, val) => Array.isArray(val) ? acc.concat(make_flat(val)) : acc.concat(val), []);
+}
+console.log("make_flat()", make_flat(mass));
+
+//1.Հաշվել զանգվածի բոլոր դրական թվերի գումարը։
+let r = [1, 2, 3, 5, 1, 0, -6];
+function m(ar){
+    let summa = 0;
+for (var i = 0; i < ar.length; i++) {
+    if(ar[i] > 0){
+        summa +=ar[i]; 
+    }
+}
+        console.log("summa", summa);
+}
+m(mass);
+
+//Առանձնացնել մասիվի դրական տվերը
+let positiv = r.filter(function(v) {
+    return v > 0; 
+  });
+ // նոր մասսիվը փոխանցել reduce Ֆունկցիային, վերադարձնելով դրական թվերի գումարը 
+let g = positiv.reduce(function(sum, val){
+  return sum + val;
+});
+    console.log("g", g);
+
+// 2. Տրված զանգվածից ստանալ նոր զանգված, որի յուրաքանչյուր էլեմենտը տրված զանգվածի էլեմենտի կրկնապատիկն է։
+let array1 = [1, 4, 65, 2.3, 3, 2, 36, 5];
+function newArrSqrt(data){
+let sqrt = data.map(x => x * 2);
+return sqrt;
+}
+console.log("newArrSqrt", newArrSqrt(array1));*/
+
+//4. Գրել ֆունկցիա, որը կգտնի երկու զանգվածների չկրկնվող էլեմենտները։
+let zangvac_1 = ["data6","data1", "data2", "data3", "data4", "data5" ];
+let zangvac_2 = ["Alpha1", "data2", "data3", "data7", "data6"];
+
+function returnObj(ob1, ob2) {
+  let z =  ob1.filter(function(val) {
+  return ob2.indexOf(val) === -1;
+});
+  let z2 = ob2.filter(function(val2) {
+  return ob1.indexOf(val2) === -1;
+ });
+return `in zangvac_1: ${z}, \nin zangvac_2: ${z2}`;
+}
+
+console.log(returnObj(zangvac_1,zangvac_2));
--- Edgar/JsWork/work/main_2.js
+++ Edgar/JsWork/work/main_2.js
@@ -1,36 +1,30 @@
 /*inch vor kod, stugel pakagcery, stugel bolor {}() bacvox pakvox pakagcery, ete stringi meja */
-let str = "dsds(cjsdb jdcjbIBC SDDK)BK{Hajastan aselis}aa[array,sasdodsi]sdsasd sa";
-
-function mainCheck(n, n1, n2) {
-    let count1, count2;
-    if (n.indexOf(n1) !== -1 && n.indexOf(n2) !== -1) {
-        count1 = n.lastIndexOf(n1);
-        count2 = n.lastIndexOf(n2);
-    } else {
-        return (`not '${n1}' or '${n2}'`);
-    }
-    let r1 = n.substring(0, count2) + n.slice(count1 + 1);
-    return ("r1", r1);
-}
-console.log(mainCheck(str, ")", "("));
-/*
-let regex1 = RegExp("[^A-Za-z0-9_,\.& ]", 'g');
-let array1;
-
-while ((array1 = regex1.exec(str)) !== null) {
-<<<<<<< HEAD
-
-  console.log(`Found ${array1[0]}. Next starts at ${regex1.lastIndex}.`);
-  // expected output: "Found foo. Next starts at 9."
-  // expected output: "Found foo. Next starts at 19."
-=======
-    console.log(`Found ${array1[0]}. Next starts at ${regex1.lastIndex}.`);
->>>>>>> 5a8e357074592abce64b7291da76f752b73f463d
-}
-console.log(array1);*/
-function regExp(arg){
-    let reg = /^([A-Z]){1,3}$)?$/;
+/*araji 3 rarery mecatax heto 2 poqr heto  _, heto xxxnn_XXXnn ...*/
+/*gtnelu enq verji pakvox }-y u heto nayenq dra pakvoxy*/
+function regEx(arg){
+    let reg = /^(((([A-Z]){3}$)?(((([A-Z]){3})([0-9]{2})$))?(((([a-z]|[A-Z]){3})([0-9]{2})[-]))?)+)$/;
     let result = reg.test(arg);
     console.log(result)
 }
 regExp("dGd90-abs66-")
+
+
+
+let str = "dsds(cjsdb jdcjbIBC SDDK)BK{Hajastan aselis}aa[array,sasdodsi]sdsasdsa";
+function mainCheck(n, n1, n2){
+	let count1, count2;
+	if(n.indexOf(n1) !== -1 && n.indexOf(n2) !== -1){
+  count1 =n.lastIndexOf(n1);
+  count2 =n.lastIndexOf(n2);
+	}else{
+		return (`not '${n1}' or '${n2}'`);
+	
+	}
+
+ console.log("count1", count1);
+ console.log("count2", count2);
+ let r1 = n.substring(0, count2) + n.slice(count1+1);
+ return("r1",r1);
+}
+console.log(mainCheck(str, ")", "("));
+
