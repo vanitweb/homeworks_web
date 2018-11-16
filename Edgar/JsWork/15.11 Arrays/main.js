9f6fecd0f9e80904d4f628f9edfb23e1065e691c
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2018-11-16 20:59:47 +0400

    16 NOV 18

--- Edgar/JsWork/15.11 Arrays/main.js
+++ Edgar/JsWork/15.11 Arrays/main.js
@@ -1,16 +1,17 @@
 let arr = ["a", [12,45,7,1,20,5], "c", "d", "e", ["name1", "name2"], 25];
 
 function copy(arg){
-const arrCopy = new Array();
-let iterator1;
+let arrCopy = new Array();
+let y;
     for(let i = 0; i<arg.length; i++){
         if(Array.isArray(arg[i])){
-            arrCopy.push(arg[i]);
-           }else{
-        arrCopy.push(arg[i]);
-           }
+          y = arg[i];
+        	arrCopy.push(y);
+            console.log("arrCopy", arrCopy);
+            return copy(y);
     }
     
+}
     return arrCopy;
 }
 console.log('MAIN', copy(arr));
@@ -18,10 +19,18 @@ console.log('MAIN', copy(arr));
 arr[1][2] = "5";
 console.log('arr', arr);
 
-function copy2(arg2){
+/*function copy2(arg2){
     let arr2 = [];
+    let y;
     const map1 = arg2.map(x => x);
-    arr2 = map1;
+        if(Array.isArray(x)){
+        	y = arg2[i];
+        	arr2.concat(y);
+            console.log("arr2", arr2);
+            return copy2(arr2);
+        
+        arr2.push(arg2[i]);
+}
     return arr2;
 }
-console.log("map :", copy2(arr));
+console.log("map :", copy2(arr));*/
