e2113fb93fcfcd0cc1beaa5d37c7d1b29fc06778
Edgar Nikoghosyan <nikogosjanedgar@gmail.com>
2018-11-20 18:08:41 +0400

    HomeWork at 20.11

--- Edgar/JsWork/Colections Map-Set 19.11/HomeWork.js
+++ Edgar/JsWork/Colections Map-Set 19.11/HomeWork.js
@@ -1,14 +1,6 @@
-let obj3 = [
-			{ readOn: "2018-Nov-18", from: "Zaven"},
-			{ readOn: "2017-Mar-05", from: "Karen"},
-			{ readOn: "2018-Apr-21", from: "Ani"},
-			{ readOn: "2018-Jun-30", from: "Jhon"}
-];
 /* Գրել ծրագիր, որը կօգնի հասկանալ, թե որ նամակներն են արդեն կարդացած։ Յուրաքանչյուր նամակ ունի տեքստ և թե ում կողմից է ուղարկված։*/
-
 //is_message_read({text: "hello”, from: "ananymous”}) => true/false
 let  myMape = new Map();
-
 let Emails = [
 			    {text: "hello Zara", from: "Davit"},
 				{text: "For Davit", from: "Zara"},
@@ -16,30 +8,32 @@ let Emails = [
 				{text: "Barev", from: "Brain"},
 ];
 
-let obj2 = {
-	read: false,
-	readOn: "2018-Nov-18",
-};
 function is_message_read(value){
 	return myMape.has(value) ? true : false;
 }
-myMape.set(Emails[0],obj3[0]);//?
-myMape.set(Emails[2],obj3[2]);
+myMape.set(Emails[0], true );//?
+myMape.set(Emails[2], true );
 console.log(is_message_read(Emails[0]));
 console.log(is_message_read(Emails[1]));
 console.log(is_message_read(Emails[2]));
 /*2.Գրել ծրագիր, որը կօգնի հասկանալ, թե որ նամակներն են արդեն կարդացած և երբ(առաջին անգամ) և ում կողմից են նրանք արդեն կարդացվել։ Յուրաքանչյուր նամակ ունի տեքստ և թե ում կողմից է ուղարկված։
 message_read_info({text: “hello”, from: “ananymous”}) => read on 2018-Nov-18 by user 1 // message is not yet read*/
-
-function message_read_info(value2){
-if (myMape.has(value2)) {
-
-console.log(JSON.stringify(myMape.get(value2)));	
-}else{
-console.log("message is not read yet");
-}
-	return ;
-}
-message_read_info(Emails[0]);
+let WhoIsRead = new Map();
+let obj3 = [
+			{ readOn: "2018-Nov-18", from: "Zaven"},
+			{ readOn: "2016-Mar-05", from: "Karen"},
+			{ readOn: "2017-Apr-14", from: "Ani"},
+			{ readOn: "2017-Jun-11", from: "Arpine"},
+];
+WhoIsRead.set(Emails[0], obj3[0]);
+WhoIsRead.set(Emails[2], obj3[1]);
+WhoIsRead.set(Emails[3], obj3[3]);
+function message_read_info(value2) {
+	if(WhoIsRead.has(value2)) {
+		console.log(Object.values(WhoIsRead.get(value2)).join(" "));
+	} else {
+		console.log("Message is not read yet:");
+	}
 message_read_info(Emails[1]);
-message_read_info(Emails[2]);
+message_read_info(Emails[0]);
+message_read_info(Emails[3]);
