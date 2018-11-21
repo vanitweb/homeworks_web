6bc59d2aa5be4458109d77776cf8bdfa12de52a1
InesaMkrtchyan <44000734+InesaMkrtchyan@users.noreply.github.com>
2018-11-21 22:49:04 +0400

    Add files via upload

---
+++ Inesa/js/js22_11_18.js.js
@@ -0,0 +1,57 @@
+homeworks
+map
+in(or a in b)?
+prototypes
+flet
+
+Маp Set
+MAP
+or let myMap = new Map();
+myMap.set("key":"value")  
+ays depqqum partadir che vor key@lini string
+myMap.size-elementneri qanak
+hertakanutyyunn pahvum e
+Set
+aystex pahvum en chkrknvox elementneri
+mySet.add(1);
+mySet.add(2);
+mySet.add(1);
+vercnum e mihat 1@  krknvoxnery chi pahvum
+kyq`https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+
+
+
+
+https://codesignal.com/
+
+
+
+
+
+
+
+
+let messages =
+[
+    {text: "Hello", from: "John"},
+    {text: "How goes?", from: "John"},
+    {text: "See you soon", from: "Alice"}
+];
+
+let readMap = new WeakMap();
+
+readMap.set(messages[0], new Date());
+readMap.set(messages[1], new Date());
+readMap.set(messages[2], new Date());
+readMap.set(messages[1], new Date());
+function read_message(user)
+{
+	console.log(messages[user]);
+}
+read_message(1);
+console.log(new Date());
