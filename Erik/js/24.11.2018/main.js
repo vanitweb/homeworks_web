15acace78001ed6736e642047f9cd8d70a3bef9c
ErikPapyan <43911612+ErikPapyan@users.noreply.github.com>
2018-11-24 19:17:11 +0400

    Update main.js

--- Erik/js/24.11.2018/main.js
+++ Erik/js/24.11.2018/main.js
@@ -1,65 +1,37 @@
-
-let message = {
-  message1: {
-   text: "My friends and family keep me grounded.",
-   from: "Bale",
- },
-  message2:  {
-   text: "I'm a dreamer. I have to dream and reach for the stars, and if I miss a star then I grab a handful of clouds.",
-   from: "Mike Tyson",
- },
-  message3 : {
-   text: "I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.'",
-   from: "Muhammad Ali",
- },
+//1.1
+Array.prototype.pop = function() {
+  return this.shift();
 };
-
-let one = new Map();
-one.set(message.message1,true);
-one.set(message.message2,true);
-one.set(message.message3,true);
-//console.log(one.get(message.message3));
-
-
-
- function fun(letter){
-   for (let mes in message) {
-     if(message[mes].text === letter.text && message[mes].from === letter.from) {
-       if (one.has(message[mes])) {
-         console.log(one.get(message[mes]));
-       }
-     }
+ let a = [1,5,6];
+ console.log(a.pop());
+  //1.2
+Array.prototype.metodner = function() {
+  for (let i in this) {
+    console.log(i);
    }
- }
- fun({text: "I'm a dreamer. I have to dream and reach for the stars, and if I miss a star then I grab a handful of clouds.", from: "Mike Tyson"});
+ };
+let a = [1,5,6];
+a.metodner()
 
-
-let namak = {
-  namak1: {
-    text1: "erb es  galu?",
-    from1: "esim",
+//2
+let oby = {
+  set set1(gnahatakan) {
+    this.gnahatakan1 = gnahatakan;
   },
-  namak2: {
-    text1: "ok",
-    from1: "esim2",
+  set set2(gnahatakan) {
+    this.gnahatakan2 = gnahatakan;
   },
-  namak3: {
-    text1: "inchka?",
-    from1: "esim1",
+  set set3(gnahatakan) {
+    this.gnahatakan3 = gnahatakan;
   },
-};
-let one1 = new Map();
-one1.set(namak.namak1,{data: "20.11.2018", from2: "Natali"});
-one1.set(namak.namak2,{data: "27.11.2018", from2: "Vatali"});
-one1.set(namak.namak3,{data: "29.11.2018", from2: "Batali"});
-// console.log(one1.get(namak.namak1));
-function func(lett) {
-  for(let i in namak) {
-    if(namak[i].text === lett.text && namak[i].from1 === lett.from1) {
-      if(one1.has(namak[i])) {
-        console.log(one1.get(namak[i]));
-      }
-    }
-  }
+  get mijin_gnahatakan() {
+    return (this.gnahatakan1 + this.gnahatakan2 + this.gnahatakan3) / 3;
+  },
+gnahatakan1 : "",
+gnahatakan2 : "",
+gnahatakan3 : "",
 }
-func({text1: "erb es galu?", from1: "esim" });
+oby.set1 = 5;
+oby.set2 = 8;
+oby.set3 = 1;
+console.log(oby.mijin_gnahatakan);
