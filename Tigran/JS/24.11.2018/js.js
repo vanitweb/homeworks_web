b56eca6d2163228d3f4483b5fc4b99615edc4c4b
QochinyanTigran <43911678+QochinyanTigran@users.noreply.github.com>
2018-11-24 13:51:40 +0300

    Update js.js

--- Tigran/JS/24.11.2018/js.js
+++ Tigran/JS/24.11.2018/js.js
@@ -2,17 +2,11 @@
 Object.prototype.forEach = function () {
 	console.log("chi ashxatum ECMA8 um");
 }
-
-
 //                         1.2
 Array.prototype.araji_element = function () {
 	console.log(this[0]);
 }
-
-
-
 //                          2
-
 let object = {
     arr : [],
     set Set(value) {
@@ -22,36 +16,9 @@ let object = {
         this.h = this.arr.length / value;
     },
     get element(){
-    	console.log(this.arr,this.h);
+		return this.arr[2]/45;
     }
 }
 object.Set = 74;
 object.Set = 45;
 object.Set = 13;
-object.kluch = "5";
-console.log(object);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//jjj
