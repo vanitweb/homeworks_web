c74a8b3a8244a774b3cb2b469098eec1e365ce70
ErikPapyan <43911612+ErikPapyan@users.noreply.github.com>
2018-11-27 19:17:51 +0400

    Update main.js

--- Erik/js/27.11.2018/main.js
+++ Erik/js/27.11.2018/main.js
@@ -4,7 +4,7 @@ function Person(name, surname, age){
   this.age = age;
 }
 Person.prototype.printInfo = function (){
-  console.log(${this.name}  ${this.surname}  ${this.age});
+  console.log(`${this.name}  ${this.surname}  ${this.age}`);
 }
 let x = new Person("Erik", "Papyan", 20);
 x.printInfo();
@@ -13,7 +13,7 @@ function Developer(name, surname, age, branch) {
   this.branch = branch;
 }
 Developer.prototype.printInfo = function () {
-  console.log(${this.name}  ${this.surname}  ${this.age} ${this.branch});
+  console.log(`${this.name}  ${this.surname}  ${this.age} ${this.branch}`);
 }
 let y = new Developer("Erik", "Papyan", 20, "JavaScript");
 y.printInfo();
@@ -22,7 +22,7 @@ function JSDeveloper(name, surname, age, branch, level) {
   this.level = level;
 }
 JSDeveloper.prototype.printInfo = function () {
-  console.log(${this.name}  ${this.surname}  ${this.age} ${this.branch} ${this.level});
+  console.log(`${this.name}  ${this.surname}  ${this.age} ${this.branch} ${this.level}`);
 }
 let z = new JSDeveloper("Erik", "Papyan", 20, "JavaScript", "junior");
 z.printInfo();
