4a4892b1f93632053bbbb372a786ffc684b3dddc
Garik71426 <garo1997@mail.ru>
2018-11-27 07:24:18 +0300

    Add 27.11.2018

---
+++ Garik/js/27.11.2018/Array_1.js
@@ -0,0 +1,44 @@
+//Գրել 3 տիպ`  Person, Developer, JSDeveloper, ժառանգել հետևյալ կերպ Person <- Developer <- JSDeveloper:
+//Երեք տիպում էլ ունենալ printInfo մեթոդ, որի մեջ կանչել ծնողի printInfo մեթոդը և տպել միայն այդ տիպին բնորոշ հատկությունները։
+function Person(anun, azganun) {
+    this.anun = anun;
+    this.azganun = azganun;
+    this.printinfo = function(){
+        for(let i in this){
+            console.log(i)
+        }
+    }
+}
+let person1 = new Person("Garik", "Iskandaryan");
+function Developer(lezu) {
+    this.lezu = lezu;
+    this.printinfo = function(){
+        for(let i in this){
+            if(this.hasOwnProperty
+              (i)){
+                console.log(i)
+            }
+        }
+    }
+}
+let developer1 = new Developer("js");
+developer1.__proto__ = person1;
+function JSDeveloper(framework) {
+    this.framework = framework;
+    this.printinfo = function(){
+        for(let i in this){
+            if(this.hasOwnProperty
+              (i)){
+                console.log(i)
+            }
+        }
+    }
+}
+let jsdeveloper1 = new JSDeveloper("Angular");
+jsdeveloper1.__proto__ = developer1;
+person1.printinfo();
+console.log(person1);
+developer1.printinfo();
+console.log(developer1);
+jsdeveloper1.printinfo();
+console.log(jsdeveloper1)
