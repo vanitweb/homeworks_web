77be3c6a18a112a0b87f47eed3d94a53e799cdda
Arevik111 <arevik-elen@mail.ru>
2018-11-27 14:29:05 +0400

    Changes in Homework 21.11

--- Arevik/Homework JS/Homework 21.11/tasks.js
+++ Arevik/Homework JS/Homework 21.11/tasks.js
@@ -8,11 +8,13 @@ console.log(myArray1.reverse());
 
 
 //Task 1.2 Ավելացնել մեր ստեղծած մեթոդը։
-let myArray2 = [2, 3, 4, 5];
+let myArray2 = [1, 3, 4, 5];
 Array.prototype.myMethod = function(){
 	let result = [];
 	for (let i = 0; i < this.length; i++) {
-		result.push(Math.pow(this[i], 3));
+		if(typeof i === "number"){
+			result.push(Math.pow(this[i], 3));
+		}
 	}
 	return result;
 }
@@ -21,22 +23,47 @@ console.log(myArray2.myMethod());
 
 
 //Task 2.Հայտարարել տիպ, որի բոլոր property-ները privat են;
+//Option1...
+function Car(model, year){
+    this.model = model;
+    this.year = year;
+}
+Car.prototype = {
+    constructor: Car,
+    get prop(){
+        return this.model + '-' + this.year;
+    },
+    set prop1(model){
+        this.model = model;
+    },
+    set prop2(year){
+        this.year = year;
+    }	
+}
+let myObject = new Car("Mersedes", 2010);
+console.log(myObject.prop);
+myObject.prop1 = "BMW";
+myObject.prop2 = 2015;
+console.log(myObject.prop);
 
-let employee = {
-    name: "",
-    surname: "",
-    get fullName() {
-        return this.name + " " + this.surname;
-    },
-    set fullName(value) {
-        let res = value.toString().split(" ");
-        this.name = res[0] || "";
-        this.surname = res[1] || "";
-    },
+
+
+//Option2...
+let func = function() {
+  let type = 'animal';
+  let sort = 'tiger';
+  let year = 25;
+
+  return {
+    get: function() { return type + '-' +sort + '-' + year ; },
+    set: function(value) { sort = value;}
+  }
 };
-employee.fullName = "Elen Sahakyan";
-console.log(employee.name);
-console.log(employee.surname);
-console.log(employee.fullName);
+
+let A = func();
+
+A.set('fish', 'ishkhan', 3);
+console.log(A.get());
+
 
 
