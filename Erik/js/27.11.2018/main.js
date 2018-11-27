5c8b7637969c64fb19176b4ee091c4e9011212de
ErikPapyan <garo1997@mail.ru>
2018-11-27 19:14:32 +0400

    homework

---
+++ Erik/js/27.11.2018/main.js
@@ -0,0 +1,28 @@
+function Person(name, surname, age){
+  this.name = name;
+  this.surname = surname;
+  this.age = age;
+}
+Person.prototype.printInfo = function (){
+  console.log(${this.name}  ${this.surname}  ${this.age});
+}
+let x = new Person("Erik", "Papyan", 20);
+x.printInfo();
+function Developer(name, surname, age, branch) {
+  Person.call(this, name, surname, age);
+  this.branch = branch;
+}
+Developer.prototype.printInfo = function () {
+  console.log(${this.name}  ${this.surname}  ${this.age} ${this.branch});
+}
+let y = new Developer("Erik", "Papyan", 20, "JavaScript");
+y.printInfo();
+function JSDeveloper(name, surname, age, branch, level) {
+  Developer.call(this, name, surname, age, branch);
+  this.level = level;
+}
+JSDeveloper.prototype.printInfo = function () {
+  console.log(${this.name}  ${this.surname}  ${this.age} ${this.branch} ${this.level});
+}
+let z = new JSDeveloper("Erik", "Papyan", 20, "JavaScript", "junior");
+z.printInfo();
