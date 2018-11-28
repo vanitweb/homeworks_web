06c55615c6b68a0d5dc31e2b7251153a375ddd39
AApinyan <arik200185@mail.ru>
2018-11-29 00:11:56 +0400

    27.11.2018

---
+++ Arusik/27.11.2018/main.js
@@ -0,0 +1,36 @@
+//Գրել 3 տիպ`  Person, Developer, JSDeveloper, ժառանգել հետևյալ կերպ Person <- Developer <- JSDeveloper:
+//Երեք տիպում էլ ունենալ printInfo մեթոդ, որի մեջ կանչել ծնողի printInfo մեթոդը և տպել միայն այդ տիպին բնորոշ հատկությունները։
+
+let persone = function() {
+	this.name = 'programmers',
+	this.surname = 'surname',
+	printInfo = function() {
+		let prop = object.cetOwnPropertyNames(persone);
+		console.log(prop);
+	};
+};
+let developer = function() {
+	this.degree = 'bachelor'
+	printInfo = function() {
+		let prop = object.cetOwnPropertyNames(developer);
+		console.log(prop);
+	};
+};
+developer.prototype = new persone;
+let jsdeveloper = function() {
+	this.programm = 'JavaScript'
+	printInfo = function() {
+		let prop = object.cetOwnPropertyNames(jsdeveloper);
+		console.log(prop);
+	};
+};
+jsdeveloper.prototype = new developer;
+let p = new persone;
+let d = new developer;
+let js = new jsdeveloper;
+js.name = 'jsdeveloper';
+//console.log(p, d, js);
+console.log(p);
+console.log(d);
+console.log(js);
+
