9cfa4613984016f18ea9cdc32acd4c810184a249
Gor Manukyan <gorman93@mail.ru>
2018-12-02 21:09:08 +0400

    marskoy boy xaxi obyektnern em porce sarqem, arayjm senc, bayc karoxa eli poxem

--- Gor/JS/Homework_29.11/marsBoy.js
+++ Gor/JS/Homework_29.11/marsBoy.js
@@ -1,2 +1,35 @@
 //Homeworks 29.11.2018
 //Marskoy boy
+console.log('******************************************************************************************************');
+class Navak {
+	constructor(navLength) {
+		this.navLength = navLength;
+	}
+}
+class Table {
+	constructor(lengthH, lengthV) {
+		this.lengthH = lengthH;
+		this.lengthV = lengthV;
+		this.navArray = new Array();
+		this.navArray.push(new Navak(1));
+		this.navArray.push(new Navak(1));
+		this.navArray.push(new Navak(1));
+		this.navArray.push(new Navak(1));
+		this.navArray.push(new Navak(2));
+		this.navArray.push(new Navak(2));
+		this.navArray.push(new Navak(2));
+		this.navArray.push(new Navak(3));
+		this.navArray.push(new Navak(3));
+		this.navArray.push(new Navak(4));
+	}
+}
+class Gamer {
+	constructor(gamerName) {
+		this.gamerName = gamerName;
+		this.table = new Table(10, 10);
+	}
+}
+let gamerOne = new Gamer('gamerOne');
+let gamerTwo = new Gamer('gamerTwo');
+console.log(gamerOne);
+console.log(gamerTwo);
