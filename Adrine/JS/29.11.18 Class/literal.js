35c0baa9e29e67da456ded8aa83c1dbbb641dc33
adset <adrine.setaghyan@mail.ru>
2018-12-04 16:03:53 +0400

    Added

---
+++ Adrine/JS/29.11.18 Class/literal.js
@@ -0,0 +1,15 @@
+// Ստեղծել օբյեկտ հետևյալ կառուցվածքով՝ Institut-> name, gradaran, 
+ let university = {
+	name: "YSU",
+	library: "Gradaran",
+	faculty: {
+		name: "Matmex",
+		Deanoffice: {
+			name:"Mathematical equations",
+			dean: "Avetisyan",
+		
+		}
+	}
+}
+console.log(university);
+console.log(university.faculty);
