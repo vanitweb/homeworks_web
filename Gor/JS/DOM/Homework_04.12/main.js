728da7b9c60fe7fddbfb21a87016762e8b316957
Gor Manukyan <gorman93@mail.ru>
2018-12-07 16:38:02 +0400

    modified homework 04.12

--- Gor/JS/DOM/Homework_04.12/main.js
+++ Gor/JS/DOM/Homework_04.12/main.js
@@ -10,3 +10,32 @@ function cssStyle() {
 		console.log(style);
 	}
 }
+
+//homework2
+
+function homework2(attr) {
+	if(attr === 0) {
+		document.getElementById("addAtrr").style.display = "block";
+		document.getElementById("change").style.display = "none";
+		document.getElementById("addElem").style.display = "none";
+		document.getElementById("printAtrr").style.display = "none";
+	}
+	if(attr === 1) {
+		document.getElementById("addAtrr").style.display = "none";
+		document.getElementById("change").style.display = "block";
+		document.getElementById("addElem").style.display = "none";
+		document.getElementById("printAtrr").style.display = "none";
+	}
+	if(attr === 2) {
+		document.getElementById("addAtrr").style.display = "none";
+		document.getElementById("change").style.display = "none";
+		document.getElementById("addElem").style.display = "block";
+		document.getElementById("printAtrr").style.display = "none";
+	}
+	if(attr === 3) {
+		document.getElementById("addAtrr").style.display = "none";
+		document.getElementById("change").style.display = "none";
+		document.getElementById("addElem").style.display = "none";
+		document.getElementById("printAtrr").style.display = "block";
+	}
+}
