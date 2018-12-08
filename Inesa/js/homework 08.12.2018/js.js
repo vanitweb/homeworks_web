b8860de0c7d63d823ad84e1d963a2778d071ae81
InesaMkrtchyan <44000734+InesaMkrtchyan@users.noreply.github.com>
2018-12-08 17:28:47 +0400

    Add files via upload

---
+++ Inesa/js/homework 08.12.2018/js.js
@@ -0,0 +1,79 @@
+var Add_element = function()
+{
+    let c = document.createElement("option");
+    document.getElementsByTagName("select").appendChild(c);
+    c.value = "Նոր էլեմենտ";
+}
+
+
+
+
+
+var AddElement = function()
+{
+    Add_element();
+}
+
+
+
+
+
+
+
+
+
+var removefunc = function()
+{
+    var r1 = document.createElement("input");
+    document.getElementsByTagName("body").appendChild(r1);//???
+    var r = document.getElementsByClassName("poxelparunakutyunn");
+    r.value = r1.value;
+    
+    
+}
+
+var remove = function()
+{
+    removefunc();
+}
+
+
+
+
+
+var Inputi_ardyunq = function()
+{
+    let x = x1.value;
+    let y = y1.value;
+    let nor_element1 = document.createElement("x");
+    let nor_element2 = document.createElement("y");
+    document.getElementsByTagName("x").appendChild("y");
+}
+let Avelacnel_2_input = function()
+{
+    let x1 = document.createElement("input");
+    let y1 = document.createElement("input");
+    x1.classname = "input1";
+    y1.classname = "input2";
+    document.getElementsByTagName("body").appendChild(x1);//???
+    document.getElementsByTagName("body").appendChild(y1);//???
+
+    Inputi_ardyunq();
+}
+let Add_atribut = function ()
+{
+    Avelacnel_2_input();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
