e60d65eae5c12488feefb6d781c8535a0c92bfc8
Gor Manukyan <gorman93@mail.ru>
2018-12-14 11:20:28 +0400

    added homeworks 14.12

---
+++ Gor/JS/homeworkShabat/14.12/refactoring.js
@@ -0,0 +1,62 @@
+//refactoring function
+/*function func(s, a, b) {
+
+   if (s.match(/^$/)) {
+       return -1;
+   }
+   
+   var i = s.length -1;
+   var aIndex =     -1;
+   var bIndex =     -1;
+   
+   while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
+       if (s.substring(i, i +1) == a) {
+           aIndex = i;
+      }
+       if (s.substring(i, i +1) == b) {
+           bIndex = i;
+      }
+       i = i - 1;
+   }
+   
+   if (aIndex != -1) {
+       if (bIndex == -1) {
+           return aIndex;
+       }
+       else {
+           return Math.max(aIndex, bIndex);
+       }
+   }
+   
+   if (bIndex != -1) {
+       return bIndex;
+   }
+   else {
+       return -1;
+   }
+}*/
+function func(s, a, b) {
+	if(s.length === 0) {
+		return -1;
+	}
+	let aIndex = -1;
+	let bIndex = -1;
+	for(let i = 1; i < s.length ; i++) {
+		if(s[i] === a) {
+			aIndex = i;
+		} else if(s[i] === b) {
+			bIndex = i;
+		}
+	}
+	if(aIndex !== -1) {
+		if(bIndex === -1) {
+			return aIndex;
+		} else {
+			return Math.max(aIndex, bIndex);
+		}
+	} else if(bIndex !== -1) {
+		return bIndex;
+	} else {
+		return -1;
+	}
+}
