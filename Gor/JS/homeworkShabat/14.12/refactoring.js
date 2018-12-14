985c854f105a497bd91a925d10c48e830ee57c53
Gor Manukyan <gorman93@mail.ru>
2018-12-14 12:16:36 +0400

    modified refactoring.js

--- Gor/JS/homeworkShabat/14.12/refactoring.js
+++ Gor/JS/homeworkShabat/14.12/refactoring.js
@@ -1,14 +1,11 @@
 //refactoring function
 /*function func(s, a, b) {
-
    if (s.match(/^$/)) {
        return -1;
-   }
-   
+   }   
    var i = s.length -1;
    var aIndex =     -1;
-   var bIndex =     -1;
-   
+   var bIndex =     -1;   
    while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
        if (s.substring(i, i +1) == a) {
            aIndex = i;
@@ -17,8 +14,7 @@
            bIndex = i;
       }
        i = i - 1;
-   }
-   
+   }   
    if (aIndex != -1) {
        if (bIndex == -1) {
            return aIndex;
@@ -26,8 +22,7 @@
        else {
            return Math.max(aIndex, bIndex);
        }
-   }
-   
+   }   
    if (bIndex != -1) {
        return bIndex;
    }
@@ -39,24 +34,7 @@ function func(s, a, b) {
 	if(s.length === 0) {
 		return -1;
 	}
-	let aIndex = -1;
-	let bIndex = -1;
-	for(let i = 1; i < s.length ; i++) {
-		if(s[i] === a) {
-			aIndex = i;
-		} else if(s[i] === b) {
-			bIndex = i;
-		}
-	}
-	if(aIndex !== -1) {
-		if(bIndex === -1) {
-			return aIndex;
-		} else {
-			return Math.max(aIndex, bIndex);
-		}
-	} else if(bIndex !== -1) {
-		return bIndex;
-	} else {
-		return -1;
-	}
+	let aIndex = s.lastIndexOf(a);
+	let bIndex = s.lastIndexOf(b);
+	return Math.max(aIndex, bIndex);
 }
