decf2c42431ffc34de376c6cd5a39862d32797b2
Karmanuk <manukyankaren00@gmail.com>
2018-12-15 20:13:28 +0400

    Update addTeg.js

--- Karen/Project/04.12.18/js/addTeg.js
+++ Karen/Project/04.12.18/js/addTeg.js
@@ -1,6 +1,6 @@
 const optionsChange = function() {
-	let ind = document.getElementById('chang').selectedIndex;
-	let name = document.getElementById('chang').options;
+	const ind = document.getElementById('chang').selectedIndex;
+	const name = document.getElementById('chang').options;
 	switch(name[ind].textContent) {
 		case 'Add atribute':
 		addAtribute();
@@ -19,9 +19,9 @@ const optionsChange = function() {
 }
 
 const addAtribute = function() {
-	let inp_1 = document.createElement('input');
-	let inp_2 = document.createElement('input');
-	let but = document.createElement('button');
+	const inp_1 = document.createElement('input');
+	const inp_2 = document.createElement('input');
+	const but = document.createElement('button');
 	but.setAttribute('onclick', 'addAtr()');
 	but.textContent = 'Create Atribute';
 	inp_1.setAttribute('id', 'inp1');
@@ -33,8 +33,8 @@ const addAtribute = function() {
 }
 
 const changeContent = function() {
-	let inp_1 = document.createElement('input');
-	let but = document.createElement('button');
+	const inp_1 = document.createElement('input');
+	const but = document.createElement('button');
 	document.body.appendChild(inp_1);
 	document.body.appendChild(but);
 	but.setAttribute('onclick', 'canCon()');
@@ -43,8 +43,8 @@ const changeContent = function() {
 }
 
 const addElement = function() {
-	let inp_1 = document.createElement('input');
-	let but = document.createElement('button');
+	const inp_1 = document.createElement('input');
+	const but = document.createElement('button');
 	document.body.appendChild(inp_1);
 	document.body.appendChild(but);
 	but.setAttribute('onclick', 'adEl()');
@@ -53,16 +53,16 @@ const addElement = function() {
 }
 
 const printAtribute = function() {
-	let atr = document.getElementById('chang').attributes;
-    for (var i = 0; i < atr.length; i++) {
+	const atr = document.getElementById('chang').attributes;
+    for (let i = 0; i < atr.length; i++) {
       alert( atr[i].name + " = " + atr[i].value );
     }
 }
 
 const addAtr = function() {
-	let name = document.getElementById('chang').options;
-	let inp1 = document.getElementById('inp1').value;
-	let inp2 = document.getElementById('inp2').value;
+	const name = document.getElementById('chang').options;
+	const inp1 = document.getElementById('inp1').value;
+	const inp2 = document.getElementById('inp2').value;
 	let x = document.createAttribute(inp1);
 	x.value = inp2;
 	name[0].setAttributeNode(x);
@@ -71,12 +71,12 @@ const addAtr = function() {
 
 const canCon = function() {
 	let inp1 = document.getElementById('inp1').value;
-	let name = document.getElementById('chang').options;
+	const name = document.getElementById('chang').options;
 	name[1].textContent = inp1;
 }
 
 const adEl= function() {
 	let inp1 = document.getElementById('inp1').value;
-	let name = document.getElementById('chang').options;
+	const name = document.getElementById('chang').options;
 	name[2].textContent = name[2].textContent + inp1;
 }
