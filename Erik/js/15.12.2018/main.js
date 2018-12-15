48e161d9e6113d2216bd272a16691a5ff9196faf
ErikPapyan <43911612+ErikPapyan@users.noreply.github.com>
2018-12-15 19:37:57 +0400

    Update main.js

--- Erik/js/15.12.2018/main.js
+++ Erik/js/15.12.2018/main.js
@@ -1,46 +1,39 @@
-function func(s, a, b) {
+// function func(s, a, b) {
  
-  if (s.match(/^$/)) {
-    return -1;
-  }
+//   if (s.match(/^$/)) {
+//     return -1;
+//   }
   
-  var i = s.length -1;
-  var aIndex =   -1;
-  var bIndex =   -1;
+//   var i = s.length -1;
+//   var aIndex =   -1;
+//   var bIndex =   -1;
   
-  while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
-    if (s.substring(i, i +1) == a) {
-      aIndex = i;
-    }
-    if (s.substring(i, i +1) == b) {
-      bIndex = i;
-    }
-    i = i - 1;
-  }
+//   while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
+//     if (s.substring(i, i +1) == a) {
+//       aIndex = i;
+//     }
+//     if (s.substring(i, i +1) == b) {
+//       bIndex = i;
+//     }
+//     i = i - 1;
+//   }
   
-  if (aIndex != -1) {
-    if (bIndex == -1) {
-      return aIndex;
-    }
-    else {
-      return Math.max(aIndex, bIndex);
-    }
-  }
+//   if (aIndex != -1) {
+//     if (bIndex == -1) {
+//       return aIndex;
+//     }
+//     else {
+//       return Math.max(aIndex, bIndex);
+//     }
+//   }
   
-  if (bIndex != -1) {
-    return bIndex;
-  }
-  else {
-    return -1;
-  }
-}
-
-
-
-
-
-
-//kisata 
+//   if (bIndex != -1) {
+//     return bIndex;
+//   }
+//   else {
+//     return -1;
+//   }
+// }
 
 function func(s, a, b) {
 	if (s.match(/^$/)) {
