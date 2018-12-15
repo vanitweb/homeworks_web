c27c03de9d5f5074ae18a2dbda5ecfc84fccade1
ErikPapyan <43911612+ErikPapyan@users.noreply.github.com>
2018-12-15 19:31:04 +0400

    Update main.js

--- Erik/js/15.12.2018/main.js
+++ Erik/js/15.12.2018/main.js
@@ -1,3 +1,45 @@
+function func(s, a, b) {
+ 
+  if (s.match(/^$/)) {
+    return -1;
+  }
+  
+  var i = s.length -1;
+  var aIndex =   -1;
+  var bIndex =   -1;
+  
+  while ((aIndex == -1) && (bIndex == -1) && (i > 0)) {
+    if (s.substring(i, i +1) == a) {
+      aIndex = i;
+    }
+    if (s.substring(i, i +1) == b) {
+      bIndex = i;
+    }
+    i = i - 1;
+  }
+  
+  if (aIndex != -1) {
+    if (bIndex == -1) {
+      return aIndex;
+    }
+    else {
+      return Math.max(aIndex, bIndex);
+    }
+  }
+  
+  if (bIndex != -1) {
+    return bIndex;
+  }
+  else {
+    return -1;
+  }
+}
+
+
+
+
+
+
 //kisata 
 
 function func(s, a, b) {
