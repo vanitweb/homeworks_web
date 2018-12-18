25dcc39d6a63436f2c8115c0416f8400bb429eb1
Tukhik Gharagyozyan <tukhik_k@mail.ru>
2018-12-18 08:40:34 +0400

    update homework

--- Tukhik/javascript/01.12.2018-x&0-n-chapani/script.js
+++ Tukhik/javascript/01.12.2018-x&0-n-chapani/script.js
@@ -1,77 +1,99 @@
 'use strict';
 let isX
+let k = 0;
+let q = 0;
+const CELL_SIZE = 10;
 const x = function() {
     let count = 0;
     isX = true;
-    let table = document.getElementsByTagName("table")[0]; 
-    let tr = document.getElementsByTagName("tr"); 
-    let td = document.getElementsByTagName("td");
     return function(e){
         if (!e.target.textContent) {
             count++;
             let cell = e.target || window.event.srcElement;
             e.target.textContent = isX ? 'X' : '0';
             isX = !isX;
+            glxavorAnkyunagits(e);
+            stugumSyuneriHamar(e);
+            yerkrordakanAnkyunagits(e)
             if(count >= 2*CELL_SIZE-1){
-                for(let i = 0; i < CELL_SIZE; i++){
-                    let qanak = 0;
-                    if(tr[e.target.cellIndex].children[i].textContent  == e.target.textContent ){
-                        qanak++
-                        if(qanak = CELL_SIZE){
-                            alert(`haxtec ${e.target.textContent} xaxacox@`);
-                                newGame();
-                                break;
-                            }
-                        }
-                    }
-                    for(let i = 0; i < CELL_SIZE; i++){
-                    let qanak2 = 0;
-                    if(tr[i].children[e.target.cellIndex].textContent === e.target.textContent  ){
-                        qanak2++
-                        if(qanak2 = CELL_SIZE){
-                            alert(`haxtec ${e.target.textContent} xaxacox@`);
-                        newGame();  
-                        break;
-                        } 
-                        }   
-                     for(let i = 0; i < CELL_SIZE; i++){
-                    if(e.target.cellIndex  === e.target.parentNode.rowIndex){
-                        let qanak3 = 0;
-                        if(tr[e.target.parentNode.rowIndex].children[e.target.cellIndex].textContent  == e.target.textContent ){
-                        qanak3++
-                            if(qanak3 = CELL_SIZE){
-                                alert(`haxtec ${e.target.textContent} xaxacox@`);
-                                newGame();
-                                break;
-                            }
-                        }
-                    }
-                    else if (e.target.cellIndex  + e.target.parentNode.rowIndex  == CELL_SIZE){
-                        let qanak4 = 0;
-                        if(tr[CELL_SIZE-i].children[i].textContent  == tr[e.target.parentNode.rowIndex].children[e.target.cellIndex].textContent ){
-                            qanak4 ++
-                            if(qanak = CELL_SIZE){
-                                alert(`haxtec ${e.target.textContent} xaxacox@`);
-                                newGame();
-                                break;
-                            }
-                         }
-                    }
-                }
+            let tbl = document.getElementsByTagName("table")[0];
+            stugumToxeriHamar(e);
             }
         }
     }
-
-let newGame =function () {
-    
-    document.getElementsByTagName('button')[0].textContent ="Try again";
-    document.getElementsByTagName('button')[0].style.display = 'block';
-    const table = document.getElementsByTagName('table')[0];
-    table.innerHTML = "";
-    isX = !isX;
+}
+function glxavorAnkyunagits(e){ 
+    let tr = document.getElementsByTagName("tr"); 
+    let td = document.getElementsByTagName("td"); 
+    let cell = e.target || window.event.srcElement; 
+    let m = cell.cellIndex
+    if(cell.cellIndex == cell.parentNode.rowIndex ) {
+        for(let m = 0; m < CELL_SIZE; m++){
+            if(cell.cellIndex == m && td[m].textContent == td[cell.cellIndex].textContent){
+                k++;
+                if(k == CELL_SIZE){
+                alert(`haxtec ${cell.textContent}-iky`);
+                newGame();
+                }
+            }
+        }
+        
+    }
 }
 
-const CELL_SIZE = 3;
+function yerkrordakanAnkyunagits(e){
+    let tr = document.getElementsByTagName("tr"); 
+    let td = document.getElementsByTagName("td");
+    let cell = e.target || window.event.srcElement; 
+    let n = cell.cellIndex;
+    if(cell.cellIndex + cell.parentNode.rowIndex == CELL_SIZE-1) {
+        for(let n = 0; n < CELL_SIZE; n++){
+            if(n + cell.parentNode.rowIndex == CELL_SIZE-1 && td[n].textContent == td[cell.cellIndex].textContent){
+            q++;
+                if(q == CELL_SIZE) {
+                alert (`haxtec ${e.target.textContent}-iky`);
+                newGame();
+                }
+            }
+        }    
+    } 
+}
+ 
+function stugumToxeriHamar (e) {
+    let tr = document.getElementsByTagName("tr"); 
+    let td = document.getElementsByTagName("td");
+    var cell = e.target || window.event.srcElement;
+    let xo = tr[cell.parentNode.rowIndex].textContent;
+        if(xo.substring(0, CELL_SIZE) == "XXXXXXXXXX" ||  xo.substring(0, CELL_SIZE) == "0000000000"){
+            alert('haxtanak');
+            newGame();
+        }
+}
+
+function stugumSyuneriHamar(e) {   
+    let tr = document.getElementsByTagName("tr"); 
+    let td = document.getElementsByTagName("td");
+    let cell = e.target || window.event.srcElement;
+    let syun= 0;
+    for(let j = 0; j < CELL_SIZE; j++){
+if(tr[j].children[e.target.cellIndex].textContent === e.target.textContent ){
+            syun++;
+            if(syun == CELL_SIZE){
+                
+            alert (`haxtecin ${td[cell.cellIndex].textContent} xaxacoxner@`);
+            newGame();
+            }
+        }
+    }
+}
+
+let newGame =function () {    
+    document.getElementsByTagName('button')[0].textContent ="NewGame";
+    document.getElementsByTagName('button')[0].style.display = 'block';
+    const table = document.getElementsByTagName('table')[0];
+    document.location.reload(true);
+}
+
 const setValue = x();
 const drawTable = function() {   
     const table = document.getElementsByTagName('table')[0];
--- Tukhik/javascript/04.12.2018-Dom/arajadranq1-option/js.js
+++ Tukhik/javascript/04.12.2018-Dom/arajadranq1-option/js.js
@@ -1,24 +1,82 @@
 
-//der uxxelu em
+//der uxxelu em, kisat e
 
-function click1() {
-        let x = document.getElementById("mySelect").selectedIndex;
-        if(x==0){ 
-        let name = document.createElement("input");
+const optionsChange = function() {
+    const ind = document.getElementById('chang').selectedIndex;
+    const name = document.getElementById('chang').options;
+    switch(name[ind].textContent) {
+        case '1. ավելացնել ատրիբուտ':
+        addAtribute();
+        break;
+        case '2. փոխել պարունակությունը':
+        changeContent();
+        break;
+        case '3. ավելացնել էլեմենտ':
+        addElement();
+        break;
+        case '4. տպել ատրիբուտները':
+        printAtribute();
+        break;
+    }
+}
+
+
+const addAtribute = function(){
+        const name = document.createElement("input");
+         const valu = document.createElement("input");
+         const but = document.createElement('button');
+         but.setAttribute('onclick', 'newAddAtribute()');
+        but.textContent = '1. ավելացնել ատրիբուտ';
+        name.setAttribute('id', 'inp1');
+        valu.setAttribute('id', 'inp2');
         document.body.appendChild(name);
-        let valu = document.createElement("input");
         document.body.appendChild(valu);
-    click() 
-        }
-         if(x==1){
- let poxelParnakutyun = document.createElement("input");
-     document.body.appendChild(poxelParnakutyun);
-         }
-        if(x==2){
-let norParnakutyun = document.createElement("input");
-     document.body.a ppendChild(norParnakutyun);
-        }
-        if(x==3){
-            
-        }
+        document.body.appendChild(but);     
+}
+
+const changeContent = function(){
+    const chenj = document.createElement("input");
+    const but = document.createElement('button');
+    document.body.appendChild(chenj);
+    document.body.appendChild(but);
+    but.setAttribute('onclick', 'newchangeContent()');
+    but.textContent = '2. փոխել պարունակությունը';
+    chenj.setAttribute('id', 'inp1');
+ }
+
+const addElement = function(){
+    const norParnakutyun = document.createElement("input");
+    const but = document.createElement('button');
+    document.body.appendChild(norParnakutyun);
+    document.body.appendChild(but);
+    but.setAttribute('onclick', 'newAddElement()');
+    but.textContent = '3. ավելացնել էլեմենտ';
+    norParnakutyun.setAttribute('id', 'inp1');
+}
+const printAtribute = function() {
+    const printAt = document.getElementById('chang').attributes;
+    for (let i = 0; i < printAt.length; i++) {
+      alert( printAt[i].name + " = " + printAt[i].value );
+    }
+}
+
+const newAddAtribute = function() {
+    const name = document.getElementById('chang').options;
+    const inp1 = document.getElementById('inp1').value;
+    const inp2 = document.getElementById('inp2').value;
+    let x = document.createAttribute(inp1);
+    x.value = inp2;
+    name[0].setAttributeNode(x);        
+}
+
+const newchangeContent = function() {
+    let inp1 = document.getElementById('inp1').value;
+    const name = document.getElementById('chang').options;
+    name[1].textContent = inp1;
+}
+
+const newAddElement= function() {
+    let inp1 = document.getElementById('inp1').value;
+    const name = document.getElementById('chang').options;
+    name[2].textContent = name[2].textContent + inp1;
 }
