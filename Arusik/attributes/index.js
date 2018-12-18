e8c52f75ad3ee54d945703a6963f31e1f5b3cb84
AApinyan <arik200185@mail.ru>
2018-12-18 18:27:53 +0400

    attributes

---
+++ Arusik/attributes/index.js
@@ -0,0 +1,17 @@
+/*
+const addAttribute = function() {}
+const changeContent = function() {}
+const addContent = function() {}
+const printAttribute = function() {}
+*/
+
+let mySelect = document.getElementById('mySelect');
+let btn = document.createElement('button');
+const clickMe = document.createTextNode('Click Me');
+function changeProperties() {
+	
+	document.body.appendChild(btn);
+	btn.appendChild(clickMe);
+}
+let input = document.createElement('input');
+
