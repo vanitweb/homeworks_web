dd51e1b1b0ae4eef8e96526c06fda82c668ea821
InesaMkrtchyan <44000734+InesaMkrtchyan@users.noreply.github.com>
2018-12-20 19:08:13 +0400

    Add files via upload

---
+++ Inesa/js/x-0/x-0/js/js.js
@@ -0,0 +1,159 @@
+var k = 0;
+
+const x = function() 
+{
+	const stugum = function()
+    {
+		let syun = document.getElementsByTagName("td");
+		let size = parseInt(document.getElementById("input").value);
+		let CELL_SIZE = size;
+		let chishtx = false;
+		let chishto = false;
+		let maxSize = CELL_SIZE * CELL_SIZE;
+		//գլխավոր անկյունագիծ
+		for(let i = 0; i <= maxSize; i += (CELL_SIZE+1))
+		{
+			if((syun[i].textContent === "X") && ((syun[i + (CELL_SIZE+1)]).textContent === "X") && ((syun[i + CELL_SIZE + CELL_SIZE + 2]).textContent === "X"))
+			{
+				chishtx = true;
+			}
+		}
+		for(let i = 0; i <= maxSize; i += (CELL_SIZE+1))
+		{
+			if((syun[i].textContent === "O") && ((syun[i + (CELL_SIZE+1)]).textContent === "O") && (syun[i + CELL_SIZE + CELL_SIZE + 2].textContent === "O"))
+			{
+				chishto = true;
+			}
+		}
+		//2րդ անկյունագիծ
+		for(let i = CELL_SIZE - 1; i <= (maxSize - CELL_SIZE) ; i += (CELL_SIZE-1))
+		{
+			if((syun[i].textContent === "X") && (syun[i + (CELL_SIZE - 1)].textContent === "X") && (syun[i + (CELL_SIZE - 1) + (CELL_SIZE - 1)].textContent === "X"))
+			{
+				chishtx = true;
+			}
+		}
+		for(let i = CELL_SIZE - 1; i <= (maxSize - CELL_SIZE) ; i += (CELL_SIZE-1))
+		{
+			if((syun[i].textContent === "O") && (syun[i + (CELL_SIZE - 1)].textContent === "O") && (syun[i + (CELL_SIZE - 1) + (CELL_SIZE - 1)].textContent === "O"))
+			{
+				chishto = true;
+			}
+		}
+		//սյուն
+		for(let j = 0; j <= (CELL_SIZE - 1); j++)
+		{
+			for(let i = j; i <= maxSize; i += CELL_SIZE)
+			{
+				if((syun[i].textContent === "X") && (syun[i + CELL_SIZE].textContent === "X") && ((syun[i + CELL_SIZE + CELL_SIZE]).textContent === "X"))
+				{
+					chishtx = true;
+				}
+			}
+		}
+		for(let j = 0; j <= (CELL_SIZE - 1); j++)
+		{
+			for(let i = j; i <= maxSize; i += CELL_SIZE)
+			{
+				if((syun[i].textContent === "O") && (syun[i + CELL_SIZE].textContent === "O") && (syun[i + CELL_SIZE + CELL_SIZE].textContent === "O"))
+				{
+					chishto = true;
+				}
+			}
+		}
+		//տող
+		for(let j = 0; j <= (maxSize - CELL_SIZE); j += CELL_SIZE)
+		{
+			for(let i = j; i <= j + (CELL_SIZE - 1); i++)
+			{
+				if((syun[i].textContent === "X") && (syun[i + 1].textContent === "X") && (syun[i + 2].textContent === "X"))
+				{
+					chishtx = true;
+				}
+			}
+		}
+		for(let j = 0; j <= (maxSize - CELL_SIZE); j += CELL_SIZE)
+		{
+			for(let i = j; i <= j + (CELL_SIZE - 1); i++)
+			{
+				if((syun[i].textContent === "O") && (syun[i + 1].textContent === "O") && (syun[i + 2].textContent === "O"))
+				{
+					chishto = true;
+				}
+			}
+		}
+		//print
+		if(chishtx === true)
+		{
+			for(let i = 0; i <= syun.length-1; i++)
+			{
+				syun[i] === "";
+			}
+			alert("Հաղթանակ X");
+		}else
+		{
+			if(chishto === true)
+			{
+				for(let i = 0; i <= syun.length-1; i++)
+				{
+					syun[i] === "";
+				}
+				alert("Հաղթանակ O");
+			}			
+		}			
+		
+    }
+	
+	
+	let count = 0;
+	let isX = true;
+	return function(e)
+    {
+		if (!e.target.textContent) 
+        {
+			count++;
+			e.target.textContent = isX ? 'X' : 'O';
+			isX = !isX;   
+		}
+        if(count >= 5)
+        {
+            stugum();
+        }
+	}
+}
+const setValue = x();
+const drawTable = function() //նկարել
+{
+    let size = parseInt(document.getElementById("input").value);
+    let CELL_SIZE = size;
+	const table = document.getElementsByTagName('table')[0];
+	for(let i = 0; i < CELL_SIZE; ++i)
+    {
+		const tr = document.createElement('tr');
+		table.appendChild(tr);
+        for(let j = 0; j < CELL_SIZE; ++j)
+        {
+            const td = document.createElement('td');
+            tr.appendChild(td);
+            td.setAttribute('onclick', 'setValue(event)');
+        }
+	}
+	let syun = document.getElementsByTagName("td");
+}
+const startGame = function ()
+{
+    drawTable();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
