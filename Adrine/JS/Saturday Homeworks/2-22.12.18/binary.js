87cd03f383a376db3d6c81bb678c2c76891159ab
adset <adrine.setaghyan@mail.ru>
2019-01-06 23:24:37 +0400

    binary search

--- Adrine/JS/Saturday Homeworks/2-22.12.18/binary.js
+++ Adrine/JS/Saturday Homeworks/2-22.12.18/binary.js
@@ -1,22 +1,18 @@
 let elements = [1, 3, 5, 7, 8, 25, 58, 128];
-
 function binary_Search(elements, value) {
-
-    let firstIndex = 0,
-
-        lastIndex = elements.length - 1,
+	let firstIndex = 0,
+		lastIndex = elements.length - 1,
         middleIndex = Math.floor((lastIndex + firstIndex) / 2);
-
-    while (elements[middleIndex] != value && firstIndex < lastIndex) {
-        if (value < elements[middleIndex]) {
-            lastIndex = middleIndex - 1;
-        } else if (value > elements[middleIndex]) {
-            firstIndex = middleIndex + 1;
-        }
-        middleIndex = Math.floor((lastIndex + firstIndex) / 2);
-    }
-
-    return (elements[middleIndex] != value) ? -1 : middleIndex;
+	while(elements[middleIndex] != value && firstIndex < lastIndex) {
+		if(value < elements[middleIndex]) {
+			lastIndex = middleIndex - 1;
+		}
+		else if(value > elements[middleIndex]) {
+			firstIndex = middleIndex + 1;
+		}
+		middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+	}
+	return (elements[middleIndex] != value) ? -1 : middleIndex;
 }
 console.log(binary_Search(elements, 128));
 console.log(binary_Search(elements, 58));
