b7db3d3fb405527f3ace3bbd42c227a68606c692
Gor Manukyan <gorman93@mail.ru>
2019-01-21 11:23:57 +0400

    der amboxjakan chi

--- Gor/JS/DOM/Shashki/shashki.js
+++ Gor/JS/DOM/Shashki/shashki.js
@@ -1,4 +1,7 @@
 const size = 8;
+let td;
+let player = 1;
+let vercracCar = {};
 let table = [
 	[0, 1, 0, 1, 0, 1, 0, 1],
 	[1, 0, 1, 0, 1, 0, 1, 0],
@@ -10,7 +13,7 @@ let table = [
 	[-1, 0, -1, 0, -1, 0, -1, 0]
 ];
 const carer = function() {
-	const td = document.getElementsByTagName('td');
+	td = document.getElementsByTagName('td');
 	for(let i = 0; i < size; i++) {
 		for(let j = 0; j < size; j++) {
 			if(table[i][j] === 1) {
@@ -44,13 +47,45 @@ const drowTable = function() {
 	}
 	carer();
 }
-
-const game = function(event){
-	const td = document.getElementsByTagName('td');
-	let row = event.target.cellIndex;
-	let col = event.target.parentNode.rowIndex;
-	console.log(row, col);
-	if(table[row][col] === -1){
-		elem.setAttribute("class","nan");
-	   }
+const emptyCar = function() {
+	delete vercracCar.x;
+	delete vercracCar.y;
+}
+const isTrueTd = function(row, col) {
+	return (row + col) % 2;
+}
+const isTrueStep = function(row, col) {
+	if(row === vercracCar.x + player && (col === vercracCar.y + 1 || col === vercracCar.y - 1)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+const firstStep = function(row, col) {
+	vercracCar.x = row;
+	vercracCar.y = col;
+}
+const secondStep = function(row, col) {
+	if(isTrueStep(row, col)) {
+		td[vercracCar.x * size + vercracCar.y].setAttribute('class', 'nan');
+		table[vercracCar.x][vercracCar.y] = 0;
+		if(player === 1) {
+			td[row * size + col].setAttribute('class', 'black');
+		} else {
+			td[row * size + col].setAttribute('class', 'white');
+		}
+		table[row][col] = player;
+		player *= -1;
+		emptyCar();
+	}
+}
+const game = function(event) {
+	let col = event.target.cellIndex;
+	let row = event.target.parentNode.rowIndex;
+	if(table[row][col] === player && !vercracCar.x && !vercracCar.y) {
+		firstStep(row, col);
+	}
+	else if(table[row][col] === 0 && (vercracCar.x || vercracCar.y) && isTrueTd(row, col)) {
+		secondStep(row, col);
+	}
 }
