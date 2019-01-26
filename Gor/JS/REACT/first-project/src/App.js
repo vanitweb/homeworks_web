96188a74d4ace13b69b3e11637deeeaee6ce24cf
Gor Manukyan <gorman93@mail.ru>
2019-01-26 21:18:28 +0400

    classWork

--- Gor/JS/REACT/first-project/src/App.js
+++ Gor/JS/REACT/first-project/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Name} from './Name';
+
+import {Info} from './Info';
+import {Input} from './Input';
 class App extends Component {
-	state={
-		name : 'Gor'
+	state = {
+		value1 : ''
 	}
-	onCangeName = () => {
-		this.setState({name:"Name1"});
+	onChangeValue = (event) => {
+		const val = event.target.value;
+		this.setState({value1:val});
 	};
+clear = () => {
+	this.setState({value1:''});
+}
   render() {
+	  const {value1} = this.state;
     return (
       <div className="App">
-		<Name name = {this.state.name} onCangeName={this.onCangeName}/>
-		<button onClick={this.onCangeName}>updateName</button>
+		<Input value = {value1} onChange = {this.onChangeValue} clearInput={this.clear}/>
+		<Info value = {value1} />
       </div>
     );
   }
