e284c90828e3e54d604df33d5642a1e2405d1794
Karen <manukyankaren00@gmail.com>
2019-01-28 23:32:26 +0400

    add homeWork

--- Karen/workreact/src/index.js
+++ Karen/workreact/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './component/26.01.19mas2/App.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
