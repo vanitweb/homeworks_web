cc28aeec35e558ee23c10ca3ce515de2895d617b
Tukhik Gharagyozyan <tukhik_k@mail.ru>
2019-01-29 01:53:02 +0400

    26.01.2019

--- Tukhik/newreact/src/App.js
+++ Tukhik/newreact/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Input} from './Input';
+import {Info} from './Info';
+/*import {FindText} from './FindText';*/
 
 class App extends Component {
+  state = {value: ""}
+  onChangeValue = (event)=>{
+    const val = event.target.value;
+    this.setState({value: val});
+  }
+  clear = () => {
+  this.setState({value:''});
+}
+// found =() => {
+//   const string = 'aajlkjjskdjfAlsj;gkejflksajfjskda'; 
+//   this.setState({value: value});
+//   const count = string.split(value).length-1; 
+// }
+
   render() {
+    const {value}=this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <Input value = {value} onChange={this.onChangeValue} clearInput={this.clear}/>
+       <Info value = {value} />
+       ///*<FindText value2 = {value} onFound={this.found} />*/
       </div>
     );
   }
