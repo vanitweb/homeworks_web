c89f5a151030779c3c66764a06ae473acc04e2e0
Gor Manukyan <gorman93@mail.ru>
2019-01-28 14:09:52 +0400

    terutyunner kan

--- Gor/JS/REACT/first-project/src/App.js
+++ Gor/JS/REACT/first-project/src/App.js
@@ -1,33 +1,50 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
-import {Main} from './Main';
-=======
+
 
 import {Info} from './Info';
 import {Input} from './Input';
->>>>>>> f90e77683c74f90f2f42ea3042079940f4e4aa16
+import {Search} from './Search';
+import {Text} from './Text';
+
 class App extends Component {
 	state = {
-		value1 : ''
+		value1 : '',
+		count : 0,
+		searchValue: '',
+		text: "asasasassgdsghfaSasasjkhgas"
+	}
+	search = (event) => {
+		const val = event.target.value;
+		this.setState({searchValue:val});
+		const txt = this.state.text;
+		const splitTxt = txt.split(this.state.searchValue);
+		this.setState({count:splitTxt.length - 1});
+	};
+	claculate = () => {
+		const txt = this.state.text;
+		const splitTxt = txt.split(this.state.searchValue);
+		this.setState({count:splitTxt.length - 1});
 	}
 	onChangeValue = (event) => {
 		const val = event.target.value;
 		this.setState({value1:val});
 	};
-clear = () => {
-	this.setState({value1:''});
-}
-  render() {
-	  const {value1} = this.state;
-    return (
-      <div className="App">
-		<Input value = {value1} onChange = {this.onChangeValue} clearInput={this.clear}/>
-		<Info value = {value1} />
-      </div>
-    );
-  }
+	clear = () => {
+		this.setState({value1:''});
+	}
+	render() {
+		const {value1, searchValue, count, text} = this.state;
+    	return (
+		  <div className="App">
+			<Input value = {value1} onChange = {this.onChangeValue} clearInput={this.clear}/>
+			<Info value = {value1} />
+			<Search value = {searchValue} onchange = {this.search} searchCount = {count}/>
+			<Text text={text}/>
+		  </div>
+		);
+  	}
 }
 
 export default App;
