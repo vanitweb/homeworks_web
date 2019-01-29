9b52f729ff7ff43ae6c7bd3d083395e59785ec37
Arevik111 <arevik-elen@mail.ru>
2019-01-29 17:03:42 +0400

    React 26.01 teri

--- Arevik/React/src/index.js
+++ Arevik/React/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Input from './Input';
+import App from './App';
 
-ReactDOM.render(<Input />, document.getElementById('test'));
+
+ReactDOM.render(<App name="react"/>, document.getElementById('test'));
 
