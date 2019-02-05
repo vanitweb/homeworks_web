25ee27434e3d1bd2b3b743f514275812b80dd003
InesaMkrtchyan <44000734+InesaMkrtchyan@users.noreply.github.com>
2019-02-05 19:07:38 +0400

    Add files via upload

---
+++ Inesa/react/jamacuyc/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Clock} from './Clock';
+
+class App extends Component {
+  render() {
+    return (
+		<Clock></Clock>
+    );
+  }
+}
+
+export {App};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
