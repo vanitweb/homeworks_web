e89e4f83e232d09c4e70a69839db4291868a67b8
InesaMkrtchyan <44000734+InesaMkrtchyan@users.noreply.github.com>
2019-02-05 20:19:20 +0400

    Update App.js

--- Inesa/react/2.05.19 calculiator/App.js
+++ Inesa/react/2.05.19 calculiator/App.js
@@ -5,46 +5,60 @@ import {Nshan} from './Nshan';
 
 class App extends Component {
 	state = {
-		tiv1:undefined,
-		tiv2:undefined,
-		ardyunq:undefined,
+		tiv1:0,
+		tiv2:0,
+		ardyunq:0,
 		gorcoxutyanNshan:"",
-		vorerordAndam:0
+		vorerordAndam:0,
 	}
-	
 	grel = (event) =>
 	{
 		this.state.vorerordAndam++;
 		if(this.state.vorerordAndam === 1)
 		{
-			this.setState({tiv1: event.target.value});
+			this.setState({tiv1: parseInt(event.target.value)});
 		}
 		if(this.state.vorerordAndam === 2)
 		{
-			this.setState({tiv2: event.target.value});
+			this.setState({tiv2: parseInt(event.target.value)});
 		}
 	}
 	
 	nshan = (event) =>
 	{
-		if(this.state.gorcoxutyanNshan==="")
-		{
-			this.setState({gorcoxutyanNshan : event.target.value});
-		}
+		this.setState({gorcoxutyanNshan : event.target.value});
 	}
 	
-	clear =() =>
+	clear = () =>
 	{
-			this.setState({tiv1 : undefined});
-			this.state.tiv2=undefined;
-			this.state.gorcoxutyanNshan="";
-			this.state.vorerordAndam = 0;
+		
+			this.setState({tiv1 : 0});
+			this.setState({tiv2 : 0});
+			this.setState({gorcoxutyanNshan : ""});
+			this.setState({ardyunq : 0});
+			this.setState({vorerordAndam : 0});
 	}
 	patasxan = () => 
 	{
 		if(this.state.gorcoxutyanNshan === "+")
 		{
-			this.setState({ardyunq:this.state.tiv1+this.state.tiv2});
+			const sum = this.state.tiv1 + this.state.tiv2;
+			this.setState({ardyunq: sum});
+		}
+		if(this.state.gorcoxutyanNshan === "-")
+		{
+			const sum = this.state.tiv1 - this.state.tiv2;
+			this.setState({ardyunq: sum});
+		}
+		if(this.state.gorcoxutyanNshan === "*")
+		{
+			const sum = this.state.tiv1 * this.state.tiv2;
+			this.setState({ardyunq: sum});
+		}
+		if(this.state.gorcoxutyanNshan === "/")
+		{
+			const sum = this.state.tiv1 / this.state.tiv2;
+			this.setState({ardyunq: sum});
 		}
 	}
   render() {
@@ -59,8 +73,8 @@ class App extends Component {
 			</div>
 			<Tver onclick={this.grel}/>
 			<Nshan onclick={this.nshan} value={this.state.gorcoxutyanNshan}/>
-			<button onclick={this.patasxan}>patasxan</button>
-			<button onclick={this.clear}>Clear</button>
+			<button onClick={this.patasxan}>patasxan</button>
+			<button onClick={this.clear}>Clear</button>
 		</div>
 	);
   }
