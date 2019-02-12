4fbe9e299edadf249a72b0111edd9824d23d2786
InesaMkrtchyan <44000734+InesaMkrtchyan@users.noreply.github.com>
2019-02-12 18:03:31 +0400

    Add files via upload

---
+++ Inesa/react/observer observable/Tver.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Tver extends Component {
+  render() {
+    return (
+	<div>
+		<div>
+			<input type="button" value={0} onClick={this.props.onclick} />
+			<input type="button" value={1} onClick={this.props.onclick} />
+			<input type="button" value={2} onClick={this.props.onclick} />
+		</div>
+		<div>
+			<input type="button" value={3} onClick={this.props.onclick} />
+			<input type="button" value={4} onClick={this.props.onclick} />
+			<input type="button" value={5} onClick={this.props.onclick} />
+		</div>
+		<div>
+			<input type="button" value={6} onClick={this.props.onclick} />
+			<input type="button" value={7} onClick={this.props.onclick} />
+			<input type="button" value={8} onClick={this.props.onclick} />
+		</div>
+		<input type="button" value={9} onClick={this.props.onclick} />
+	</div>
+	)
+  }
+}
+
+export {Tver};
