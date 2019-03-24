448b3b3ec59acb5103d63206fc510283894e1382
Gor Manukyan <gorman93@mail.ru>
2019-03-24 23:31:12 +0400

    not worked

--- Gor/JS/NodeJS/first_app/routes/users.js
+++ Gor/JS/NodeJS/first_app/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const jsonfile = require('jsonfile');
+const file = './data/users.json';
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  jsonfile.readFile(file, function (err, obj) {
+  if (err) console.error(err)
+  console.dir(obj)
+})
 });
 
 module.exports = router;
